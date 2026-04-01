dd22b44d84d174ac9321bba112655916df343070
Nicholas-Merillat <nicholasmerillat4@gmail.com>
2026-04-01 00:49:48 -0400

    Player

---
+++ Player.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TileMap
+{
+    internal class Player
+    {   
+        public Vector2 position;
+
+        public Player(Vector2 position)
+        {
+            this.position = position;
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw()
+        {
+            
+        }
+    }
+}
