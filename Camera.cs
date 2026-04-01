8760afdeefc1c08683c183d6ed2e422b94553c60
Nicholas-Merillat <nicholasmerillat4@gmail.com>
2026-03-31 23:42:46 -0400

    Add project files.

---
+++ Camera.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TileMap
+{
+    internal class Camera
+    {
+        public Vector2 position;
+
+        private int cameraSpeed = 5;
+
+        public Camera(Vector2 position)
+        {
+            this.position = position;
+        }
+
+        public void Update()
+        {   
+
+            // Input
+            if (Keyboard.GetState().IsKeyDown(Keys.A))
+            {
+                position.X -= cameraSpeed;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.D))
+            {
+                position.X += cameraSpeed;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+            {
+                position.Y -= cameraSpeed;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.S))
+            {
+                position.Y += cameraSpeed;
+            }
+
+            // Boundaries
+            if (position.X < 0)
+            {
+                position.X = 0;
+            }
+            if (position.Y < 0)
+            {
+                position.Y = 0;
+            }
+        }
+    }
+}
---
+++ TileMap.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TileMap
+{
+    internal class TileMap
+    {
+        public Vector2 size;
+        public int[,] grid;
+
+        public TileMap(Vector2 size)
+        {
+            this.size = size;
+            grid = new int[(int)size.X, (int)size.Y];
+        }
+    }
+}
